b6e02221401d692ead86ea141d73dcafafc0fbe8
hydraChaosu <abmallim@gmail.com>
2019-06-06 16:44:26 +0200

    Add woking scenario

--- dna/dna.js
+++ dna/dna.js
@@ -1,24 +1,24 @@
 import { ModuleMap } from "jest-haste-map";
 
-const CODONS = [
-  { AUG: "Methionine" },
-  { UUU: "Phenylalanine" },
-  { UUC: "Phenylalanine" },
-  { UUA: "Leucine" },
-  { UUG: "Leucine" },
-  { UCU: "Serine" },
-  { UCC: "Serine" },
-  { UCA: "Serine" },
-  { UCG: "Serine" },
-  { UAU: "Tyrosine" },
-  { UAC: "Tyrosine" },
-  { UGU: "Cysteine" },
-  { UGC: "Cysteine" },
-  { UGG: "Tryptophan" },
-  { UAA: false },
-  { UAG: false },
-  { UGA: false }
-];
+const CODONS = {
+  AUG: "Methionine",
+  UUU: "Phenylalanine",
+  UUC: "Phenylalanine",
+  UUA: "Leucine",
+  UUG: "Leucine",
+  UCU: "Serine",
+  UCC: "Serine",
+  UCA: "Serine",
+  UCG: "Serine",
+  UAU: "Tyrosine",
+  UAC: "Tyrosine",
+  UGU: "Cysteine",
+  UGC: "Cysteine",
+  UGG: "Tryptophan",
+  UAA: false,
+  UAG: false,
+  UGA: false
+};
 
 const rnaTranslate = RNA => {
   if (RNA.length % 3 !== 0) return "RNA have incorrect length";
@@ -26,7 +26,11 @@ const rnaTranslate = RNA => {
   for (let i = 0; i < RNA.length; i = i + 3) {
     codonsFromRNA.push(RNA.slice(i, i + 3));
   }
-  const proteins = codonsFromRNA.map();
+  const proteins = codonsFromRNA.map(codon => {
+    if (CODONS.hasOwnProperty(codon)) {
+      return CODONS[codon];
+    }
+  });
   return proteins;
 };
 
